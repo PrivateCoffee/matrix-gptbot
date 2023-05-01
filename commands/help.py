5ea32b2295c6195ecc020aea43a3a6bdd378c86b
Kumi <git@kumi.email>
2023-05-01 08:43:25 +0000

    Slightly improve ordering of commands in help text

--- commands/help.py
+++ commands/help.py
@@ -6,18 +6,18 @@ async def command_help(room: MatrixRoom, event: RoomMessageText, bot):
     body = """Available commands:
 
 - !gptbot help - Show this message
+- !gptbot botinfo - Show information about the bot
+- !gptbot privacy - Show privacy information
 - !gptbot newroom \<room name\> - Create a new room and invite yourself to it
 - !gptbot stats - Show usage statistics for this room
-- !gptbot botinfo - Show information about the bot
-- !gptbot coin - Flip a coin (heads or tails)
-- !gptbot ignoreolder - Ignore messages before this point as context
 - !gptbot systemmessage \<message\> - Get or set the system message for this room
+- !gptbot coin - Flip a coin (heads or tails)
 - !gptbot imagine \<prompt\> - Generate an image from a prompt
 - !gptbot calculate [--text] [--details] \<query\> - Calculate a result to a calculation, optionally forcing text output instead of an image, and optionally showing additional details like the input interpretation
-- !gptbot privacy - Show privacy information
 - !gptbot chat \<message\> - Send a message to the chat API
 - !gptbot classify \<message\> - Classify a message using the classification API
 - !gptbot custom \<message\> - Used for custom commands handled by the chat model and defined through the room's system message
+- !gptbot ignoreolder - Ignore messages before this point as context
 """
 
     await bot.send_message(room, body, True)
