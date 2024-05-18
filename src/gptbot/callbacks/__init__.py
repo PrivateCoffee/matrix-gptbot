df567d005e394553f46d72eba575c23beddc4072
Kumi <git@kumi.email>
2024-05-18 21:36:06 +0200

    refactor(callbacks): remove test callbacks and imports
    This commit streamlines the `callbacks` module by removing the debugging and testing related callbacks (`test_callback` and `test_response_callback`) along with their associated imports. It focuses on enhancing the clarity and maintainability of the callback handling by eliminating unused code paths and dependencies that were specifically used for testing purposes. This cleanup is part of an effort to mature the codebase and reduce potential confusion for new contributors by ensuring that only operational code is present in the production paths. This should not affect the existing functionality but will make future modifications and understanding of the callback mechanisms more straightforward.

--- src/gptbot/callbacks/__init__.py
+++ src/gptbot/callbacks/__init__.py
@@ -1,31 +1,23 @@
 from nio import (
     RoomMessageText,
     InviteEvent,
-    Event,
     SyncResponse,
     JoinResponse,
     RoomMemberEvent,
-    Response,
-    MegolmEvent,
-    KeysQueryResponse
 )
 
-from .test import test_callback
 from .sync import sync_callback
 from .invite import room_invite_callback
 from .join import join_callback
 from .message import message_callback
 from .roommember import roommember_callback
-from .test_response import test_response_callback
 
 RESPONSE_CALLBACKS = {
-    #Response: test_response_callback,
     SyncResponse: sync_callback,
     JoinResponse: join_callback,
 }
 
 EVENT_CALLBACKS = {
-    #Event: test_callback,
     InviteEvent: room_invite_callback,
     RoomMessageText: message_callback,
     RoomMemberEvent: roommember_callback,
